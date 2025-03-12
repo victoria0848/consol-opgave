bb326f84c92796ab8e0dabd4c2c7f214c919fd87
victoria0848 <viku5@elev.techcollege.dk>
2025-03-12 10:59:46 +0100

    added some console command to my js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,10 @@
 // write cool JS here!!
+console. group('Console opgave');
+var name = 'Jeg er en almindelig konsol log';
+console.log('Jeg er en almindelig konsol log');
+console.info('Jeg er en info konsol log');
+console.warn('Jeg er en warning konsol log');
+console.error('Jeg er en error konsol log');
 const myInfo = 'en variabel';
 const myBooks = ["The Catcher in the Rye",
     "To Kill a Mockingbird",
@@ -10,4 +16,7 @@ const myBooks = ["The Catcher in the Rye",
     "The Lord of the Rings",
     "The Da Vinci Code",
     "The Hunger Games"]
-
+console.table(myBooks);
+console.time('default');
+console.timeEnd('default');
+console.groupEnd();
